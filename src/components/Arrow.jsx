82b0faf84ee0d586a226eab51224f0e4f9ff5586
MauroMDU <mauro.umfier@hotmail.com>
2023-06-28 10:31:55 -0300

    Hice toda la tare 1

---
+++ src/components/Arrow.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Arrow() {
+  return (
+    <div>
+        <div>
+            <img src="/src/assets/images/arrowleft" alt="" />
+        </div>
+        <div>
+            <img src="/src/assets/images/arrowrigth" alt="" />
+        </div>
+    </div>
+  )
+}
